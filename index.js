317d59f0dd41b405a293b9459fc4cda1f6d43258
BetulKilincaslan <82607026+BetulKilincaslan@users.noreply.github.com>
2021-06-30 20:52:48 +0300

    deneme 2

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 let data = [];
+let formDOM = document.querySelector("#filterForm");
+let cbAdult = document.querySelector("#cbAdult");
+let cbActive = document.querySelector("#cbActive");
 
 const fetchData = () => {
-  //verinin çekildiği yer
   fetch("data.json")
     .then((response) => {
       return response.json();
     })
     .then((responseData) => {
-      //json'dan okunan verinin data array'ine atanması
       data = responseData;
-
-      //veri geldikten sonra filtreleme butonu görünür olsun
-      let listingOptions = document.querySelector(".table");
-      listingOptions.classList.toggle("d-none");
+      formDOM.classList.toggle("d-none");
 
       //verinin html içerisinde listelendiği fonksiyon
       listData(responseData);
@@ -22,25 +20,20 @@ const fetchData = () => {
       console.log(err);
     });
 };
-
 //verinin ul tag'i içerisinde listelenmesini sağlayan fonksiyon
 const listData = (data) => {
   let list = document.querySelector(".list");
   list.classList.add("fw-bold");
   list.innerHTML = data.map((element) => {
     return `
-        <li class="d-flex flex-column" id=${element.id}>
-        <span>Name : ${element.name} </span> 
-        <span>email : ${element.age}</span>
-        <span>IsActive : ${element.isActive}</span>
-        </li>
-        `;
+          <li class="d-flex flex-column" id=${element.id}>
+          <span>Name : ${element.name} </span>
+          <span>email : ${element.age}</span>
+          <span>IsActive : ${element.isActive}</span>
+          </li>
+          `;
   });
 };
-
-// Filter Data Function
-let cbAdult = document.querySelector("#cbAdult");
-let cbActive = document.querySelector("#cbActive");
 
 const filterData = (filter) => {
   switch (filter) {
@@ -65,9 +58,16 @@ const filterData = (filter) => {
         (item) => item.name.charAt(0) === inputValue.toUpperCase()
       );
       listData(matchUsers);
-
       break;
     default:
       break;
   }
 };
+filterData();
+
+formDOM.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log("asdj");
+}
