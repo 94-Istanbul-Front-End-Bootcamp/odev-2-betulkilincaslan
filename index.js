b08a08afaf37be22c99e1d05a7aa37a953cdda5f
BetulKilincaslan <82607026+BetulKilincaslan@users.noreply.github.com>
2021-07-01 19:11:12 +0300

    Changed filtering options

--- index.js
+++ index.js
@@ -1,73 +1,92 @@
 let data = [];
-let formDOM = document.querySelector("#filterForm");
-let cbAdult = document.querySelector("#cbAdult");
-let cbActive = document.querySelector("#cbActive");
 
 const fetchData = () => {
+  // Fetching data from data.json file
   fetch("data.json")
     .then((response) => {
       return response.json();
     })
     .then((responseData) => {
+      //json data
       data = responseData;
-      formDOM.classList.toggle("d-none");
-
-      //verinin html içerisinde listelendiği fonksiyon
+      // Show listing options after click get button
+      let listingOptions = document.querySelector(".table");
+      listingOptions.classList.toggle("d-none");
+      // Function for listing data in HTML
       listData(responseData);
     })
     .catch((err) => {
-      //hata yönetimi
+      // Error handling
       console.log(err);
+      alert("Bir hata oluştu!");
     });
 };
-//verinin ul tag'i içerisinde listelenmesini sağlayan fonksiyon
+// Function for listing elements
 const listData = (data) => {
   let list = document.querySelector(".list");
   list.classList.add("fw-bold");
   list.innerHTML = data.map((element) => {
     return `
-          <li class="d-flex flex-column" id=${element.id}>
-          <span>Name : ${element.name} </span>
-          <span>email : ${element.age}</span>
-          <span>IsActive : ${element.isActive}</span>
-          </li>
-          `;
+        <li class="d-flex flex-column" id=${element.id}>
+        <span>Name : ${element.name} </span> 
+        <span>Age : ${element.age}</span>
+        <span>IsActive : ${element.isActive}</span>
+        </li>
+        `;
   });
 };
 
-const filterData = (filter) => {
-  switch (filter) {
-    case "isActive":
-      let activeUsers = data.filter((item) => item.isActive == true);
-      cbActive.checked == true ? listData(activeUsers) : listData(data);
-      break;
-    case "adult":
-      let adultUsers = data.filter((item) => item.age >= 18);
-      cbAdult.checked == true ? listData(adultUsers) : listData(data);
-      break;
-    //Burada her ikisi de check olunca ortak filtreleme yapsın istedim yemedi :)
-    // case "activeAdult":
-    //   let activeAdultUsers = data.filter(
-    //     (item) => item.age >= 18 && item.isActive == true
-    //   );
-    //   listData(activeAdultUsers);
-    //   break;
-    case "nameFilter":
-      let inputValue = document.querySelector("#inputValue").value;
-      let matchUsers = data.filter(
-        (item) => item.name.charAt(0) === inputValue.toUpperCase()
+// Filtering Data Function
+let filterButton = document.querySelector("#filterButton");
+
+filterButton.addEventListener(
+  "click",
+  (filterData = () => {
+    let cbAdultChecked = document.querySelector("#cbAdult").checked;
+    let cbActiveChecked = document.querySelector("#cbActive").checked;
+    let inputValue = document.querySelector("#inputValue").value;
+    let filteredData = data;
+
+    if (cbAdultChecked) {
+      filteredData = data.filter((item) => item.age >= 18);
+    }
+    if (cbActiveChecked) {
+      filteredData = data.filter((item) => item.isActive === true);
+    }
+    if (cbAdultChecked && cbActiveChecked) {
+      filteredData = data.filter(
+        (item) => item.isActive === true && item.age >= 18
       );
-      listData(matchUsers);
-      break;
-    default:
-      break;
-  }
-};
-filterData();
-
-formDOM.addEventListener("submit", handleSubmit);
-
-function handleSubmit(e) {
-  e.preventDefault();
-  console.log("asdj");
-}
+    }
+    if (inputValue) {
+      filteredData = data.filter(
+        (item) => item.name.charAt(0).toUpperCase() === inputValue.toUpperCase()
+      );
+    }
+    if (cbAdultChecked && inputValue && !cbActiveChecked) {
+      filteredData = data
+        .filter(
+          (item) =>
+            item.name.charAt(0).toUpperCase() === inputValue.toUpperCase()
+        )
+        .filter((item) => item.age >= 18);
+    }
+    if (cbActiveChecked && inputValue && !cbAdultChecked) {
+      filteredData = data
+        .filter(
+          (item) =>
+            item.name.charAt(0).toUpperCase() === inputValue.toUpperCase()
+        )
+        .filter((item) => item.isActive === true);
+    }
+    if (cbAdultChecked && cbActiveChecked && inputValue) {
+      filteredData = data
+        .filter(
+          (item) =>
+            item.name.charAt(0).toUpperCase() === inputValue.toUpperCase()
+        )
+        .filter((item) => item.isActive === true && item.age >= 18);
+    }
+    listData(filteredData);
+  })
+);
